48a0dfdc51ca74a153b470a28852b150930e3e57
mrjwit <mike.r.j.wit@gmail.com>
2023-04-14 18:08:56 +0200

    include right dilutions

--- R/data_clean/clean_ecoli.R
+++ R/data_clean/clean_ecoli.R
@@ -55,7 +55,7 @@ d <- ecoli %>%
   # select only relevant columns
   select(samplecode, parameter, value, units, notes)
 
-# check duplo values
+# # check duplo values
 duplos <- d %>%
   filter(str_detect(notes, "duplo")) %>%
   view()
@@ -125,7 +125,15 @@ d2 <- d2 %>%
          detection_limit = NA,
          sd = NA,
          method = "Petrifilm plate") %>%
+  ungroup() %>%
   select(samplecode, parameter, value, sd, limit_symbol, detection_limit, units, method, notes) %>%
+  mutate(notes = case_when(
+         samplecode == "GW098" ~ "large air bubble, 1 Petrifilm not fully incubated",
+         samplecode == "SR028" ~ "1 Petrifilm diluted 1:10",
+         samplecode == "WW005" ~ "1 petrifilm undiluted, another diluted 1:10",
+         samplecode %in% c("WW004", "WW006", "WW007") ~ "1 petrifilm diluted 1:10 and another 1:100",
+         TRUE ~ notes)) %>%
+  distinct() %>%
   arrange(samplecode)
 
 ## merge datasets together ##
@@ -134,7 +142,7 @@ dat <- rbind(d, d2)
 # Check if every sample has only 1 value
 check <- dat %>%
   group_by(samplecode) %>%
-  dplyr::summarise(measurements = n_distinct(value)) %>%
+  dplyr::summarise(measurements = length(value)) %>%
   filter(measurements > 1)
 if(nrow(check) > 0) {
   stop("More than 1 value for E.coli in a sample")

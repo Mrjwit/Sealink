d45b3b6c1e8be74466e78463056455ea504d2385
mrjwit <mike.r.j.wit@gmail.com>
2023-08-17 14:45:43 +0200

    Added IHE datafile and plotted the 18O/2H, but yet without sd!

--- R/data_clean/clean_isotopes.R
+++ R/data_clean/clean_isotopes.R
@@ -19,7 +19,7 @@
 
 # Loading packages
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, openxlsx)
+pacman::p_load(tidyverse, openxlsx, scales)
 
 ###############################################################################
 # load data
@@ -28,7 +28,7 @@ pacman::p_load(tidyverse, openxlsx)
 # set data file location
 input <- "C:/Users/mikewit/Documents/SEALINK/Data/Raw_data/Lab/Isotopes/" 
 
-# isotopes Summary data files
+# isotopes Summary data files for deuterium and 18-oxygen from TUD lab
 d1 <- read.xlsx(paste0(input, "W-6362--W-6381_2H_BriefSummary.xlsx")) # samples 
 d2 <- read.xlsx(paste0(input, "W-6392--W-6401_2H_BriefSummary.xlsx"))
 d3 <- read.xlsx(paste0(input, "W-6427--W-6451_2H_BriefSummary.xlsx"))
@@ -39,7 +39,7 @@ O2 <- read.xlsx(paste0(input, "W-6392--W-6401_18O_BriefSummary.xlsx"))
 O3 <- read.xlsx(paste0(input, "W-6427--W-6451_18O_BriefSummary.xlsx"))
 O4 <- read.xlsx(paste0(input, "W-6572--W-6581_18O_BriefSummary.xlsx"))
 
-# isotopes raw data files
+# isotopes raw data files from TUD lab
 dr1 <- read.xlsx(paste0(input, "W-6362--W-6381_2H.xlsx")) # samples 
 dr2 <- read.xlsx(paste0(input, "W-6392--W-6401_2H.xlsx"))
 dr3 <- read.xlsx(paste0(input, "W-6427--W-6451_2H.xlsx"))
@@ -49,6 +49,9 @@ Or1 <- read.xlsx(paste0(input, "W-6362--W-6381_18O.xlsx"))
 Or2 <- read.xlsx(paste0(input, "W-6392--W-6401_18O.xlsx"))
 Or3 <- read.xlsx(paste0(input, "W-6427--W-6451_18O.xlsx"))
 Or4 <- read.xlsx(paste0(input, "W-6572--W-6581_18O.xlsx"))
+
+#isotopes from IHE lab
+IHE <- read.xlsx(paste0(input, "/IHE/Overview_isotopes_IHE.xlsx"))
 
 # output file location
 output <- "C:/Users/mikewit/Documents/SEALINK/Data/" 
@@ -120,7 +123,7 @@ d18O <- oxygen18 %>%
 ggplot(d18O, aes(x = waarde1, y = waarde2)) +
   geom_point() +
   geom_abline(slope = 1, linetype = "dashed") +
-  #geom_smooth(method = 'lm', formula = y~x) +
+  geom_smooth(method = 'lm', formula = y~x) +
   scale_x_continuous(name = expression(paste(delta ^{18}, "O", "(\u2030)", " value 1"))) +
   scale_y_continuous(name = expression(paste(delta ^{18}, "O", "(\u2030)", " value 2"))) +
   # coord_cartesian(xlim = c(-3, -0.5),
@@ -266,9 +269,98 @@ ggplot(d_wide %>% filter(sampletype != "demi-water"), aes(x = value_d18O, y = va
   coord_cartesian(xlim = c(-3.1, 1.76),
                   ylim = c(-20, 15)) +
   theme_bw() + 
-  theme(legend.position = c(0.15, 0.78))
+  theme(legend.position = c(0.15, 0.78)) +
+  ggtitle("Stable water isotopes (TUD) - incomplete")
 ggsave(paste0(output, "Output/Figures/Hydrochemistry/isotopes_incomplete.png"),
-       width = 8, height = 8)
+       width = 6, height = 6)
+
+# Change IHE data
+d_IHE <- IHE %>%
+  select(samplecode, sampletype, year, EC, `δ18O,.in.‰`, `δ2H,.in.‰`, d2H.sd, d18O.sd) %>%
+  rename(value_d18O = `δ18O,.in.‰`, 
+         value_d2H = `δ2H,.in.‰`) 
+
+## Plot IHE, values still unreliable and without SD!!
+# plot
+ggplot(d_IHE, aes(x = value_d18O, y = value_d2H)) +
+  # errorbar 2H
+  # geom_errorbar(aes(ymin = value_d2H - sd_d2H, ymax = value_d2H + sd_d2H),
+  #               width = 0.05, colour = "grey") +
+  # errorbar 18O
+  # geom_errorbar(aes(xmin = value_d18O - sd_d18O, xmax = value_d18O + sd_d18O),
+  #               width = 0.4, colour = "grey") + 
+  geom_abline(aes(slope = 8.02, intercept = 9.47, colour = "GMWL"),
+              linetype = "dashed", show.legend = TRUE) +
+  # geom_abline(aes(slope = 5.575918, intercept = 1.815, colour = "TWML"),
+  #             linetype = "dashed", show.legend = T) +
+  geom_abline(aes(slope = 6.976744, intercept = 7.94186, colour = "LMWL?"),
+              linetype = "dashed", show.legend = T) +
+  geom_point(aes(colour = sampletype, shape = sampletype)) +
+  # geom_abline(aes(slope = , intercept = , colour = "LMWL"),
+  #             linetype = "dashed", show.legend = TRUE) +
+  #scale_fill_manual(name = "", values = c("blue")) +
+  #scale_colour_manual(name = "", values = c("red", "steelblue", "green", "purple")) +
+  #geom_text(hjust = 0, vjust = 2) +
+  # scale_colour_brewer(name = "", palette = "Set1",
+  #                     breaks = c("GMWL", "LMWL?", "TWML", "groundwater", "surface runoff", "tapwater"),
+  #                     guide = guide_legend(
+  #                       override.aes = list(pch = c(NA, NA, NA, 16, 16, 16), linetype = c(2, 2, 2, 0, 0, 0))
+  #                     )) +
+  scale_shape_manual(name = "", values = c(16, 16, 16, 16, 16, 16, 16, 16),
+                     guide = "none") +
+  scale_colour_manual(name = "", values = c("black", "#619CFF", "#00BE22", "blue1", "red", "forestgreen", 
+                                            "#00A9FF99", "#CD9600", "#FF61CC", "#C77CFF"),
+                      breaks = c("GMWL", "LMWL?", "groundwater", "rainwater", "seawater", "spring", 
+                                 "surface runoff", "surface water", "tapwater", "wastewater"),
+                      guide = guide_legend(
+                        override.aes = list(pch = c(NA, NA, 16, 16, 16, 16, 16, 16, 16, 16), linetype = c(2, 2, 0, 0, 0, 0, 0, 0, 0, 0))
+                      )) +
+  scale_x_continuous(name = expression(paste(delta ^{18}, "O", "(\u2030)", " vs VSMOW"))) +
+  scale_y_continuous(name = expression(paste(delta ^{2}, "H", "(\u2030)", " vs VSMOW"))) +
+  # coord_cartesian(xlim = c(-3.1, 1.76),
+  #                 ylim = c(-20, 15)) +
+  theme_bw() + 
+  theme(legend.position = c(0.15, 0.7)) +
+  ggtitle("Stable water isotopes Curacao (IHE) - incomplete")
+ggsave(paste0(output, "Output/Figures/Hydrochemistry/isotopes_IHE_incomplete.png"),
+       width = 6, height = 6)
+
+## Stable isotopes with EC
+# plot
+ggplot(d_IHE %>% mutate(EC = as.numeric(EC)), aes(x = value_d18O, y = value_d2H)) +
+  # errorbar 2H
+  # geom_errorbar(aes(ymin = value_d2H - sd_d2H, ymax = value_d2H + sd_d2H),
+  #               width = 0.05, colour = "grey") +
+  # errorbar 18O
+  # geom_errorbar(aes(xmin = value_d18O - sd_d18O, xmax = value_d18O + sd_d18O),
+  #               width = 0.4, colour = "grey") + 
+  geom_abline(aes(slope = 8.02, intercept = 9.47, colour = "GMWL"),
+              linetype = "dashed", show.legend = TRUE) +
+  # geom_abline(aes(slope = 5.575918, intercept = 1.815, colour = "TWML"),
+  #             linetype = "dashed", show.legend = T) +
+  geom_abline(aes(slope = 6.976744, intercept = 7.94186, colour = "LMWL?"),
+              linetype = "dashed", show.legend = T) +
+  geom_point(aes(colour = sampletype, size = EC), alpha = 0.7) +
+  scale_shape_manual(name = "", values = c(16, 16, 16, 16, 16, 16, 16, 16),
+                     guide = "none") +
+  scale_colour_manual(name = "", values = c("black", "#619CFF", "#00BE22", "blue1", "red", "forestgreen", 
+                                            "#00A9FF99", "#CD9600", "#FF61CC", "#C77CFF"),
+                      breaks = c("GMWL", "LMWL?", "groundwater", "rainwater", "seawater", "spring", 
+                                 "surface runoff", "surface water", "tapwater", "wastewater"),
+                      guide = guide_legend(
+                        override.aes = list(pch = c(NA, NA, 16, 16, 16, 16, 16, 16, 16, 16), linetype = c(2, 2, 0, 0, 0, 0, 0, 0, 0, 0))
+                      )) +
+  scale_size_continuous(guide = guide_legend(override.aes = list(linetype = c(0, 0, 0, 0, 0)))) +
+  scale_x_continuous(name = expression(paste(delta ^{18}, "O", "(\u2030)", " vs VSMOW"))) +
+  scale_y_continuous(name = expression(paste(delta ^{2}, "H", "(\u2030)", " vs VSMOW"))) +
+  # coord_cartesian(xlim = c(-3.1, 1.76),
+  #                 ylim = c(-20, 15)) +
+  theme_bw() + 
+  theme(legend.position = c(0.12, 0.65)) +
+  ggtitle("Stable water isotopes Curacao (IHE) - incomplete")
+
+
+show_col(hue_pal()(8))
 
 ###############################################################################
 # save data
